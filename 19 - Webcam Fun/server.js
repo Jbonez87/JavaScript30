ee6ad3cc00e18df16403f9d7b505580776a92715
jbonez87 <jcastrillon087@gmail.com>
2018-05-27 17:29:47 -0400

    update srcObject and add express server

---
+++ 19 - Webcam Fun/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
